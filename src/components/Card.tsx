688b63db135d6d5ec7c76433e6ba93bcda25f1bb
ADEDANCE <uthmanadedance@gmail.com>
2026-08-30 22:59:50 +0100

    add icon

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,33 @@
+import type { IconType } from "react-icons";
+
 interface CardProps {
   number?: string;
   title: string;
-  explanation: string;
+  explanation?: string;
+  icon?: IconType;
 }
 
-export const Card = ({ number, title, explanation }: CardProps) => {
+export const Card = ({
+  number,
+  title,
+  explanation,
+  icon: Icon,
+}: CardProps) => {
   return (
     <div className="bg-slate px-3 py-3 rounded-2xl p-6 w-full">
-      {/* Number - only rendered if provided */}
+      {/* Icon  */}
+      {Icon && <Icon className="text-indigo mb-3" size={24} />}
+
+      {/* Number */}
       {number && <p className="text-indigo">{number}</p>}
 
       {/* Title */}
       <p className="text-off-white text-xl font-semibold">{title}</p>
 
       {/* Explanation */}
-      <p className="text-slate-400 mt-2">{explanation}</p>
+      {explanation && (
+        <p className="text-slate-400 mt-2">{explanation}</p>
+      )}
     </div>
   );
 };
