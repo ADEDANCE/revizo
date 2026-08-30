c8eb42a47d2023fd5dfe9b05f4918a8f961c817e
ADEDANCE <uthmanadedance@gmail.com>
2026-08-30 11:57:51 +0100

    Feat: import the new sections

--- src/pages/Landingpage.tsx
+++ src/pages/Landingpage.tsx
@@ -1,9 +1,13 @@
 import { Hero } from "../sections/Hero";
+import { HowItWork } from "../sections/HowItWork";
+import { Tools } from "../sections/Tools";
 
 export const Landingpage = () => {
   return (
     <div className=" bg-deep-navy w-full">
       <Hero />
+      <HowItWork />
+      <Tools />
     </div>
   );
 };
