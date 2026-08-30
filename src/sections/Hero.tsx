ccf73ac896c81af255cea8bb439f2fc89876a542
ADEDANCE <uthmanadedance@gmail.com>
2026-08-30 11:58:47 +0100

    Refractor: hide the line on mobile screen

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,20 +1,34 @@
 import Button from "../components/Button";
+import HeroImage from "../assets/Images/heroImage.png";
+import { useNavigate } from "react-router";
 
 export const Hero = () => {
+  const navigate = useNavigate();
   return (
-    <section className=" w-full px-6 py-7 bg-deep-navy">
+    <section className=" w-full px-6 py-7 ">
       <div className=" mx-auto text-center">
-        <h1 className=" text-off-white text-4xl
-         md:text-7xl">
-          Study smarter with the <br /> material you already have.
+        <h1
+          className=" text-off-white text-4xl
+         md:text-7xl"
+        >
+          Study smarter with the <br className=" hidden md:block" /> material
+          you already have.
         </h1>
 
-        <p className=" text-slate-400">
+        <p className=" text-slate-400 mt-6  md:text-2xl">
           Upload your lecture materials and let Revizo turn them into
           explanations, summaries, practice questions, and flashcards.
         </p>
 
-        <Button className=" bg-indigo">Start Studying </Button>
+        <Button className=" bg-indigo mt-5" onClick={() => navigate("/signup")}>
+          Start Studying{" "}
+        </Button>
+
+        <img
+          src={HeroImage}
+          alt="Revizo study dashboard"
+          className="mt-6 mx-auto"
+        />
       </div>
     </section>
   );
