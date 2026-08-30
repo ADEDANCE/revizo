471c069a4b06d387f855ee7e414a8d1540ce4122
ADEDANCE <uthmanadedance@gmail.com>
2026-08-30 22:18:03 +0100

    Feat: Import sidebar

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -1,54 +1,13 @@
+import { SidebarComponent } from "@/components/SidebarComponent";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Outlet } from "react-router";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarProvider,
-} from "@/components/ui/sidebar";
-
 const DashboardLayout = () => {
   return (
     <SidebarProvider>
       {" "}
-      <Sidebar>
-        {" "}
-        <SidebarContent>
-          {" "}
-          <SidebarGroup>
-            {" "}
-            <SidebarGroupLabel>Revizo</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                <SidebarMenuItem>
-                  <SidebarMenuButton>Dashboard</SidebarMenuButton>
-                </SidebarMenuItem>
-
-                <SidebarMenuItem>
-                  <SidebarMenuButton>Materials</SidebarMenuButton>
-                </SidebarMenuItem>
-
-                <SidebarMenuItem>
-                  <SidebarMenuButton>Study Tools</SidebarMenuButton>
-                </SidebarMenuItem>
-
-                <SidebarMenuItem>
-                  <SidebarMenuButton>History</SidebarMenuButton>
-                </SidebarMenuItem>
-
-                <SidebarMenuItem>
-                  <SidebarMenuButton>Settings</SidebarMenuButton>
-                </SidebarMenuItem>
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-      </Sidebar>
-      <main className="flex-1">
+      <SidebarComponent />
+      <main className="flex-1 bg-deep-navy text-off-white">
+        <SidebarTrigger />
         <Outlet />
       </main>
     </SidebarProvider>
