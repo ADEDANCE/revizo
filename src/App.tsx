707aeb06dc3b235f2af5693db7fcef004687ffc9
ADEDANCE <uthmanadedance@gmail.com>
2026-08-30 12:33:41 +0100

    Install shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Navbar } from "./components/Navbar";
 import { Landingpage } from "./pages/Landingpage";
+import Footer from "./sections/Footer";
+import Signup from "./pages/Signup";
+import { Login } from "./pages/Login";
 
 const App = () => {
   return (
@@ -8,7 +11,10 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Landingpage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
